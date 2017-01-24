c67ea34d7d27602999d221de09d6101eb2217e11
dan <dgolant@gmail.com>
2017-01-24 00:03:20 -0500

    post-jason

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
 import requests
 import simplejson as json
 from flask import Flask
-from sqlalchemy import *
+from sqlalchemy import *  # look into flake8 for linting and library comments
 from datetime import datetime
 import dateutil.parser
+from contextlib import contextmanager
+
+# correcter way to do strings string = "foobar{baz}".format(baz="var")
+
+
 
 #Timer stuff, need to validate necessity
 from apscheduler.schedulers.background import BackgroundScheduler
@@ -13,7 +18,6 @@ sched = BackgroundScheduler()
 app = Flask(__name__)
 
 #Helpers
-
 def load_config_settings():
 	with open('config.json') as json_data:
 		settings = json.load(json_data)
@@ -23,7 +27,8 @@ def load_config_settings():
 settings = load_config_settings()
 news_api_key = settings["news_api_key"]
 
-newsSources = ['associated-press',
+newsSources = [
+	'associated-press',
 	'bbc-news',
 	'bloomberg',
 	'cnn',
@@ -32,58 +37,77 @@ newsSources = ['associated-press',
 	'the-new-york-times',
 	'the-telegraph',
 	'the-wall-street-journal',
-	'the-washington-post'
+	'the-washington-post',
 ]
 
 #SQL Connection
-CONNECTION_STRING = 'mysql://' + settings["mysqluser"] + ':' + settings["mysqlpw"] + '@' + settings["mysqllocation"] + '/' + settings["database"]+ '?charset=utf8&use_unicode=0'
+CONNECTION_STRING = (
+	'mysql://' + settings["mysqluser"] + ':' + settings["mysqlpw"] + '@'
+	+ settings["mysqllocation"] + '/' + settings["database"] +
+	'?charset=utf8&use_unicode=0'
+)
 engine = create_engine(CONNECTION_STRING, pool_recycle=3600)
 connection = engine.connect()
 
 
 def get_news(source, sort, key):
-	newsAPIURL = 'https://newsapi.org/v1/articles?source='+source+'&sortBy='+sort+'&apiKey='+key
-	r = None
+	newsAPIURL = (
+		'https://newsapi.org/v1/articles?source=' + source + '&sortBy='
+		 + sort + '&apiKey=' + key
+	)
 	try:
-		r = requests.get(newsAPIURL).content
-		return r
-	except requests.exceptions.RequestException:
-		print("Error fetching")
+		return requests.get(newsAPIURL).content
+	except requests.exceptions.RequestException as e:
+		print("Error fetching: {}".format(e))
+
 
 def get_multisource_news_array(sources, sort):
-	jsonNewsList = []
+	json_news_list = []
 	for source in sources:
-		jsonNewsList.append(get_news(source, sort, news_api_key).decode('utf8'))
-	return jsonNewsList
+		json_news_list.append(get_news(source, sort, news_api_key).decode('utf8'))
+	return json_news_list
 
-def format_data(jsonContent):
+
+def build_url_list(parsed_json):
+	anchor = "<a href={url}>{title}</a>"
+	for article in parsed_json["articles"]:
+		yield anchor.format(
+			url=article["url"],
+			title=article["title"],
+		)
+
+
+def format_data(json_content):
 	# TODO: return more than just titles
-	parsedJson = json.loads(jsonContent)
-	if parsedJson["status"] == "ok":
-		lines = ""
-		for article in parsedJson["articles"]:
-			lines += "<a href=" + article["url"] + ">" + article["title"] + "</a>" + "<br/>"
+	parsed_json = json.loads(json_content)
+	if parsed_json["status"] == "ok":
+		lines = "<br/>".join(build_url_list(parsed_json))
 		return lines
-	else:
-		return "status == not ok"
+	return "status == not ok"
+
 
 @app.route('/')
 def hello():
     return "Hello World!"
 
+@contextmanager
+def sql_execute(query, *args, **kwargs):
+	try:
+		result = engine.execute(query, *args, **kwargs)
+		yield result
+	except Exception:
+		result.close()
+
 @app.route('/unsortedlist')
 def list_articles():
-	result = engine.execute("select * from UnsortedArticles")
-	lines = ""
-	for row in result:
-		lines += "<a href=" + row["url"].decode('utf8') + ">" + row["title"].decode('utf8') + "</a>" + "<br/>"
-	result.close()
+	with sql_execute("select * from UnsortedArticles") as result:
+		lines = "<br/>".join.join(build_url_list(result))
 	return lines
 
 
 def fetch_articles_and_save():
-	newsJSONArray = get_multisource_news_array(newsSources, "top")
-	for source in newsJSONArray:
+	news_json_array = get_multisource_news_array(newsSources, "top")
+	for source in news_json_array:
 		source = json.loads(source)
 		for article in source["articles"]:
 			if article["publishedAt"] is not None:
@@ -91,9 +115,11 @@ def fetch_articles_and_save():
 				ods = ods[:19]
 				dt = datetime.strptime(ods, "%Y-%m-%dT%H:%M:%S")
 				dts = dt.strftime('%Y-%m-%d %H:%M:%S')
-				engine.execute ("""INSERT INTO UnsortedArticles (author, title, url, imageurl, description, publishtime) VALUES (%s,%s, %s, %s, %s, %s)""", article["author"], article["title"], article["url"], article["urlToImage"], article["description"], dts)
+				with sql_execute("""INSERT INTO UnsortedArticles (author, title, url, imageurl, description, publishtime) VALUES (%s,%s, %s, %s, %s, %s)""", article["author"], article["title"], article["url"], article["urlToImage"], article["description"], dts):
+					pass
 			else:
-				engine.execute ("""INSERT INTO UnsortedArticles (author, title, url, imageurl, description) VALUES (%s,%s, %s, %s, %s)""", article["author"], article["title"], article["url"], article["urlToImage"], article["description"])
+				with sql_execute("""INSERT INTO UnsortedArticles (author, title, url, imageurl, description) VALUES (%s,%s, %s, %s, %s)""", article["author"], article["title"], article["url"], article["urlToImage"], article["description"]):
+					pass
 	return "complete"
 
 
