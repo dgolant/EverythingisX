1337f9e725a586b9d0956e325f3b6db4350174cd
dan <dgolant@gmail.com>
2017-01-24 22:57:33 -0500

    modified log formats

--- app.py
+++ app.py
@@ -5,9 +5,10 @@ from sqlalchemy import *  # look into flake8 for linting and library comments
 from datetime import datetime
 import dateutil.parser
 from contextlib import contextmanager
+import coloredlogs, logging
 
-# correcter way to do strings string = "foobar{baz}".format(baz="var")
-
+logger = logging.getLogger(__name__)
+coloredlogs.install(level='INFO')
 
 
 #Timer stuff, need to validate necessity
@@ -58,13 +59,14 @@ def get_news(source, sort, key):
 	try:
 		return requests.get(newsAPIURL).content
 	except requests.exceptions.RequestException as e:
-		print("Error fetching: {}".format(e))
+		logger.Error("Error fetching: {}".format(e))
 
 
 def get_multisource_news_array(sources, sort):
 	json_news_list = []
+	logger.info("News source request sent!")
 	for source in sources:
-		json_news_list.append(get_news(source, sort, news_api_key).decode('utf8'))
+		json_news_list.append((get_news(source, sort, news_api_key)).decode('utf8'))
 	return json_news_list
 
 
@@ -94,13 +96,15 @@ def hello():
 def sql_execute(query, *args, **kwargs):
 	try:
 		result = engine.execute(query, *args, **kwargs)
+		logging.info("{query_operation} EXECUTED".format(query_operation=query[:6]))
 		yield result
 	except Exception:
 		result.close()
 
 @app.route('/unsortedlist')
 def list_articles():
-	with sql_execute("select * from UnsortedArticles") as result:
+	lines = None
+	with sql_execute("SELECT * FROM UnsortedArticles") as result:
 		lines = "<br/>".join.join(build_url_list(result))
 	return lines
 
@@ -125,7 +129,7 @@ def fetch_articles_and_save():
 
 
 # seconds can be replaced with minutes, hours, or days
-sched.add_job(fetch_articles_and_save, trigger = 'cron', day = '*', hour = 0)
+sched.add_job(fetch_articles_and_save, trigger = 'cron', day = '*', hour = '0')
 sched.start()
 
 
